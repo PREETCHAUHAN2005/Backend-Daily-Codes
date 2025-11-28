f81bf53c18f479e1796be7c58705bd18a2fbacf8
Preet Chauhan <preet.24b0101197@abes.ac.in>
2025-11-28 23:54:21 +0530

    Refactor BLOCKING.js for clarity and structure

--- BLOCKING.js
+++ BLOCKING.js
@@ -2,16 +2,16 @@ const crypto = require("node:crypto");
 var a = 3;
 var b = 5;
 
-// pbkdf2 : password based key derivative function
 
-//  Synchronous function block the ma in thread do not use it
+
+
 crypto.pbkdf2Sync("password", "salt", 500000, 50, "preet");
 console.log("Key is generated");
 
 crypto.pbkdf2("password", "salt", 500000, 50, "preet", (error, key) => {
   console.log("Key is generated");
 });
-//
+
 setTimeout(() => {
   console.log("call me right now");
 }, 0);
@@ -23,3 +23,4 @@ function mult(x, y) {
 var c = mult();
 console.log(c);
 // Password based key derivative function2
+
